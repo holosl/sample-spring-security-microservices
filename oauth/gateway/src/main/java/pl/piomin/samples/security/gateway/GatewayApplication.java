e3927de466890e057f33d339f1811fafb3f04030
abu <abu@home>
2024-04-03 13:39:21 +0200

    programmatic token relay

--- oauth/gateway/src/main/java/pl/piomin/samples/security/gateway/GatewayApplication.java
+++ oauth/gateway/src/main/java/pl/piomin/samples/security/gateway/GatewayApplication.java
@@ -2,6 +2,11 @@ package pl.piomin.samples.security.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
@@ -18,6 +23,8 @@ public class GatewayApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
 
+    @Autowired
+    private TokenRelayGatewayFilterFactory filterFactory;
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
@@ -30,6 +37,22 @@ public class GatewayApplication {
     @GetMapping("/")
     public Mono<String> index(WebSession session) {
         return Mono.just(session.getId());
+    }
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("callme-service", p -> p
+                        .path("/callme/**")
+                        .filters(f-> f.filter(filterFactory.apply()))
+                        .uri("http://localhost:8040"))
+                .route("caller-service", p -> p
+                        .path("/caller/**")
+                        .filters(f-> f.filter(filterFactory.apply()))
+                        .uri("http://localhost:8020"))
+
+                .build();
     }
 
 }
