6ae7a3290d505091aa4dec72fbfb1b41f0453876
abu <abu@home>
2024-04-03 11:48:14 +0200

    enable scope check

--- oauth/gateway/src/test/java/pl/piomin/samples/security/gateway/CallmeController.java
+++ oauth/gateway/src/test/java/pl/piomin/samples/security/gateway/CallmeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/callme")
 public class CallmeController {
 
-    @PreAuthorize("hasAuthority('SCOPE_TEST')")
     @GetMapping("/ping")
     public String ping() {
         return "Hello!";
