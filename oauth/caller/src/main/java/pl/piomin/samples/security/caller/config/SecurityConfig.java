973c333def5397d3b1cd3e06434fdeaf0ac995ab
abu <abu@home>
2024-04-03 12:23:46 +0200

    deprecated EnableGlobalMethodSecurity

--- oauth/caller/src/main/java/pl/piomin/samples/security/caller/config/SecurityConfig.java
+++ oauth/caller/src/main/java/pl/piomin/samples/security/caller/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package pl.piomin.samples.security.caller.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
