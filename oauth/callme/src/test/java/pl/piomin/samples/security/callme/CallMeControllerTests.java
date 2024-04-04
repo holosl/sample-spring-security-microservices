0844ea4af6c43e0b73384e2da1ff761dd54ae0cf
abu <abu@home>
2024-04-04 12:59:08 +0200

    add authority check

--- oauth/callme/src/test/java/pl/piomin/samples/security/callme/CallMeControllerTests.java
+++ oauth/callme/src/test/java/pl/piomin/samples/security/callme/CallMeControllerTests.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.piomin.samples.security.common.Scope;
+
+import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,5 +27,20 @@ public class CallMeControllerTests {
     @Test
     void whenNoAuthorityScopeRequiredThenAuthenticationIsEnough() throws Exception {
         this.mockMvc.perform(get("/callme/pong").with(jwt())).andExpect(status().isOk());
+    }
+    @Test
+    void whenLackingScopeThenUnauthorized() throws Exception {
+        this.mockMvc.perform(get("/callme/ping").with(jwt())).andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenHavingScopeThenOk() throws Exception {
+        Jwt tokenWithClaim = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("sub", "user")
+                .claim("scope", "read")
+                .claim("scope", Scope.c9)
+                .build();
+        this.mockMvc.perform(get("/callme/ping").with(jwt().authorities(List.of(new SimpleGrantedAuthority(Scope.c9))))).andExpect(status().isOk());
     }
 }
